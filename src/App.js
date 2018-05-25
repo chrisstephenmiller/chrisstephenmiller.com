f289b070a92349f9ae568261592eaf627b0de852
chrisstpehenmiller <csmiller4567@gmail.com>
2018-05-24 23:25:39 -0500

    background and name

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="city">
+          <div className="background">
+            <span className="name">chris stephen miller</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
