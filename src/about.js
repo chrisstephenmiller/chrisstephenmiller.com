b0282df554bec4fe49fdd9dec4cce8a7acfb41aa
chrisstpehenmiller <csmiller4567@gmail.com>
2018-06-04 01:49:24 -0500

    all album embeds

--- src/about.js
+++ src/about.js
@@ -5,9 +5,7 @@ class About extends Component {
   render() {
     return (
       <div className="about route">
-        <span className="title text">I am a web-developer and fullstack engineer living in Chicago, Illinois.</span>
-        <span className="detail text">My passion is building interactive and engaging software that encouarges creativity and collaboration.</span>
-        <span className="detail text">Music has always been a significant presence in my life. I grew up playing multiple instruments and, as a teenager, began producing music using computers. After studying Music Technology at Northwestern University, I managed customer support teams for multiple music/audio tech companies, where I developed a desire to build software. I incorporate music in many of my programming projects and enjoy inventing innovative tools for musical creation.</span>
+          <span className="title text">I am a web-developer, musician, and audio engineer living in Chicago, Illinois.</span>
       </div>
     );
   }
--- src/music.js
+++ src/music.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import './routes.css'
 
+const albums = [
+  { name: `whysowhite`, link: `180341696` },
+  { name: `pvhfcs`, link: `2188654707` },
+  { name: `scary-blankets`, link: `4001214234` },
+]
+
 class Music extends Component {
   render() {
     return (
       <div className="music route">
-        <span className="title text">This is where I write about music.</span>
-        <iframe 
-        src="https://bandcamp.com/EmbeddedPlayer/album=2188654707/size=large/bgcol=ffffff/linkcol=5eff00/minimal=true/transparent=true/"
-        id="iframe"
-        allow="autoplay"
-        />
+        <span className="title text">These are albums I recorded.</span>
+        <div className="albums">
+          {albums.map(album => {
+            return (
+              <div>
+                <iframe
+                  src={`https://bandcamp.com/EmbeddedPlayer/album=${album.link}/size=large/bgcol=ffffff/linkcol=5eff00/minimal=true/`}
+                  key={album.name}
+                  className={`album ${album.name}`}
+                  allow="autoplay"
+                  seamless
+                />
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
