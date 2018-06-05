5d3affd103044dc48af16bec3c53a4f05b2462f0
chrisstpehenmiller <csmiller4567@gmail.com>
2018-06-05 00:43:02 -0500

    code projects

--- src/code.js
+++ src/code.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import './routes.css'
+import './code.css'
+import guitars from './guitars.gif'
+import jamSession from './session.gif'
+import dwellplayd from './dwellplayd.gif'
+
+
 
 class Code extends Component {
   render() {
     return (
       <div className="code route">
         <span className="title text">This is where I write about code.</span>
+        <div className="projects">
+          <img className="jam-session embed" src={jamSession} />
+          <img className="guitars embed" src={guitars} />
+          <img className="dwellplayd embed" src={dwellplayd} />
+        </div>
       </div>
     );
   }
