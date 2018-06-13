46a85ff7a4c2e129064a75ba6530924c74658378
chrisstpehenmiller <csmiller4567@gmail.com>
2018-06-13 16:49:36 -0500

    album decriptions

--- src/components/music.js
+++ src/components/music.js
@@ -4,17 +4,51 @@ const projects = [
   {
     name: `whysowhite - whysowhite (2013)`,
     link: `180341696`,
-    text: `this is a short description, no more than a few lines.`
+    text: `recorded and mixed by chris stephen miller
+additional recording by steve rashid
+mastered by tom tierney
+artwork by devin soisson
+    
+big nick — vocals 
+charlie moonbeam — vocals 
+d-pop fantastic — vocals, congas 
+dngr — lead guitar 
+uncle funk — rhythm guitar 
+custom sumberg — bass 
+fatphlams levine — drums 
+brahmz — percussion 
+the shef — keys, vocals`
   },
   {
     name: `the positive vibration high frequency calibration sequencer - whysowhite (2015)`,
     link: `2188654707`,
-    text: `this is a medium description. there will be more than a few lines and some details included about the project.`
+    text: `recorded and mixed by chris stephen miller
+mastered by rick fritz
+artwork by dav yendler
+    
+nick mcmillan — vocals, percussion
+davis haines — vocals, percussion
+charles haines — vocals
+chris miller — guitar, keys
+sean carolan — bass
+andrew abramowitz — drums
+sam taylor — guitar
+charlie dwyer — guitar
+hannah shefsky — keys, vocals
+sir jove — vocals`
   },
   {
-    name: `Scary Blankets - Mungion (2016)`,
+    name: `scary blankets - mungion (2016)`,
     link: `4001214234`,
-    text: `this is a long description. there will be a number of lines, significant details, and extra information. it will take up much more real estate than the other descriptions.`
+    text: `recorded and mixed by chris stephen miller
+additional recording by matt jordan
+mastered by rick fritz 
+artwork by russell moore and tiffany vedua 
+        
+justin reckamp — guitar, vox
+joe re — organ, synth, piano, vox
+sean carolan — upright, electric bass, vox
+david collum — drums, percussion, vox`
   },
 ]
 
@@ -36,7 +70,7 @@ class Music extends Component {
                   frameBorder="0"
                 />
                 <span className="subtitle text">{project.name}</span>
-                <span className="info text">{project.text}</span>
+                {project.text.split(`\n`).map(line => <span className="info text">{line}</span>)}
               </div>
             )
           })}
